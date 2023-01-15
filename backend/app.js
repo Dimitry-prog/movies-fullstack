3fab62e126ee0da0e2920be99a8ef87b42e9f960
Dimitry-prog <kfifa3@gmail.com>
2023-01-15 22:27:31 +0300

    feat: app packages

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import appRouter from './routes/index.js';
 import handleErrors from './middlewares/handleErrors.js';
+import { RATE_LIMIT } from './utils/constants.js';
 
 const PORT = 3000;
 const DB_URL = 'mongodb://localhost:27017/bitfilmsdb';
 const app = express();
 
+app.use(helmet());
+app.use(RATE_LIMIT);
+app.use(cookieParser());
 app.use(express.json());
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63c40944f242c799a0d9fd42',
-  };
-
-  next();
-});
 
 app.use(appRouter);
 
+app.use(errors());
 app.use(handleErrors);
 
 const startApp = async () => {
