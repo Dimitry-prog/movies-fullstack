5c437b234b92c4c2bf58938afe46a6fb515ec71b
Dimitry-prog <kfifa3@gmail.com>
2023-01-17 23:24:01 +0300

    refac: header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,23 +5,42 @@ import profileIcon from '../../images/profile_icon.svg';
 import {Link, NavLink} from 'react-router-dom';
 
 const Header = () => {
+    const isAuth = false;
+    // const isAuth = true;
+
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${isAuth ? `${styles.header_hero}` : ''}`}>
             <Link to='/' className={styles.header__logo}><img src={logo} alt="logo"/></Link>
-            <nav>
-                <ul className={styles.header__list}>
-                    <li>
-                        <NavLink to='/films'>Фильмы</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/favoriute'>Сохранённые фильмы</NavLink>
-                    </li>
-                </ul>
-                <button>
-                    Аккаунт
-                    <img src={profileIcon} alt="profile"/>
-                </button>
-            </nav>
+
+            {!isAuth && (
+                <nav className={styles.header__nav_hero}>
+                    <ul className={styles.header__list_hero}>
+                        <li className={styles.header__item_hero}>
+                            <Link to='/signup'>Регистрация</Link>
+                        </li>
+                        <li className={`${styles.header__item_hero} ${styles.header__item_hero_signin}`}>
+                            <Link to='/signin'>Войти</Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+
+            {isAuth && (
+                <nav className={styles.header__nav}>
+                    <ul className={styles.header__list}>
+                        <li className={styles.header__item}>
+                            <NavLink to='/films'>Фильмы</NavLink>
+                        </li>
+                        <li className={styles.header__item}>
+                            <NavLink to='/favoriute'>Сохранённые фильмы</NavLink>
+                        </li>
+                    </ul>
+                    <button>
+                        Аккаунт
+                        <img src={profileIcon} alt="profile"/>
+                    </button>
+                </nav>
+            )}
         </header>
     );
 };
---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+
+const Hero = () => {
+    return (
+        <section className={styles.hero}>
+
+        </section>
+    );
+};
+
+export default Hero;
