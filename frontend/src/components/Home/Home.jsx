f3fdd91edd39f47a9beced18062734cec365e056
Dimitry-prog <kfifa3@gmail.com>
2023-01-18 18:27:11 +0300

    feat: techs section

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './Home.module.scss';
 import Hero from '../Hero/Hero';
 import About from '../About/About';
+import Techs from '../Techs/Techs';
 
 const Home = () => {
     return (
         <main className={styles.home}>
             <Hero/>
             <About/>
+            <Techs/>
         </main>
     );
 };
