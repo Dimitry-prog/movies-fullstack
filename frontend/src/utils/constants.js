3ad134e4903eb3d5f98a5ed3c7dcaa6dfc601431
Dimitry-prog <kfifa3@gmail.com>
2023-02-11 23:13:19 +0300

    fix: url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://api.last-diplom.nomoredomains.rocks';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://api.last-diplom.nomoredomains.rocks';
+// export const BASE_URL = 'http://localhost:5000';
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const IMG_BASE_URL = 'https://api.nomoreparties.co';
 export const SHORT_MOVIE_DURATION = 40;
